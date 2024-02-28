21630d67eebbef37e8f470e485d6114e7aa10a7d
Prabin <prabinparajuli496@gmail.com>
2024-02-28 20:20:14 +0545

    Added search bar functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
+import React, { useState } from 'react'; // Import useState for conditional rendering
 import logo from './Rotation.png';
 import './App.css';
 import { useAuth0 } from "@auth0/auth0-react";
-import Login from './login';
+import SearchBar from './SearchBar'; // Import your SearchBar component
 
 function App() {
   const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
-  console.log("Current User" , user);
+  const [isSearchOpen, setIsSearchOpen] = useState(false); // State for search bar visibility
+
+  const toggleSearch = () => setIsSearchOpen(!isSearchOpen); // Function to toggle search bar
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-      
 
-        {/* Show username if user is logged in */}
-        <ul className='userNameShow'>{isAuthenticated && <p> Welcome {user.name}</p>}</ul>
+        {/* Username and Search bar conditionally rendered */}
+        {isAuthenticated && (
+          <>
+            <ul className='userNameShow'><p> Welcome {user.name}</p></ul>
+            <SearchBar isOpen={isSearchOpen} toggleSearch={toggleSearch} />
+          </>
+        )}
 
-        {/* Show login button if user is not logged in, otherwise show logout button */}
+        {/* Login/Logout button */}
         {isAuthenticated ? (
           <ul>
             <button className='LogInButton' onClick={() => logout({ returnTo: window.location.origin })}>Logout</button>
@@ -25,12 +32,11 @@ function App() {
           <ul>
             <button className='LogInButton' onClick={() => loginWithRedirect()}>Log In</button>
           </ul>
-        
         )}
       </header>
-      <div>
-        <Login />
-      </div>
+
+      {/* Push-to-Talk button conditionally rendered */}
+      {isAuthenticated && <button className='PushToTalkButton'>Push to Talk</button>}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
-import React from 'react';
+import React from 'react'; // Import React from 'react' module
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
 
-
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<Auth0Provider
+  <Auth0Provider
     domain="dev-et2icwt8cvpvgpxs.us.auth0.com"
     clientId="znP44AzUkkVdULRBIWCoXwQ5bHfHa3AI"
     authorizationParams={{
