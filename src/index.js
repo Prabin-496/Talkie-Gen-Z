919520126678dca65b8575735297abefda4f39ca
Prabin <prabinparajuli496@gmail.com>
2024-03-01 09:56:23 +0545

    client Id and domain name corrected

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Auth0Provider
-    domain="dev-8luaqlehtj00eoxi.us.auth0.com"
-    clientId="lJL7wlZyyJqHAs4VVSJomYeTmKTkoYuH"
+    domain="dev-et2icwt8cvpvgpxs.us.auth0.com"
+    clientId="znP44AzUkkVdULRBIWCoXwQ5bHfHa3AI"
     authorizationParams={{
       redirect_uri: window.location.origin
     }}
