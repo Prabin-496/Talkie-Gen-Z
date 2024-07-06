2cf4f49455ec28894dfe9f72592dfa5b83e818e7
Prabin <prabinparajuli496@gmail.com>
2024-07-06 22:24:10 +0545

    AI

--- src/index.js
+++ src/index.js
@@ -3,23 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
+import Chatbot from './Chatbot.jsx';
+
+// import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
 // import TalkButton from './talkbutton';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Auth0Provider
-    domain="talkie-gen-z.us.auth0.com"
-    clientId="YkdnPqgJnF1HcArmTcu3E7FaxHWWEWKV"
-    authorizationParams={{
-      redirect_uri: window.location.origin
-    }}
-  >
+  // <Auth0Provider
+  //   domain="talkie-gen-z.us.auth0.com"
+  //   clientId="YkdnPqgJnF1HcArmTcu3E7FaxHWWEWKV"
+  //   authorizationParams={{
+  //     redirect_uri: window.location.origin
+  //   }}
+  // >
+  <Chatbot>
     <App />
-    {/* <TalkButton /> */}
-  </Auth0Provider>,
+   </Chatbot >
+  // </Auth0Provider>,
 );
 
 reportWebVitals();
