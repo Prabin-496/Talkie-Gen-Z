75592c3c9bfc01c8c89be5e7b74ea17e2833e239
Prabin <prabinparajuli496@gmail.com>
2024-07-07 12:50:08 +0545

    FIX csss

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,23 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build'
-  }
+    outDir: 'build',
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.').at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+      },
+    },
+  },
+  css: {
+    // CSS preprocessing configurations can go here
+  },
 })
